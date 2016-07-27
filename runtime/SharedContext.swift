a233727a5398a239dc3e07708b5fbf02d5bd197c
Krzysztof Kaczor <chris@kaczor.io>
2016-07-27 19:52:45 +0200

    improved shared context (again...)

--- runtime/SharedContext.swift
+++ runtime/SharedContext.swift
@@ -2,23 +2,16 @@ import Foundation
 import JavaScriptCore
 
 struct SharedTypeSwiftContext {
-    static var once = dispatch_once_t()
+    static let sharedInstance = SharedTypeSwiftContext()
+    let context = JSContext()
 
-    private static let _defaultContext = JSContext()
-
-    static let defaultContext = {
-        () -> JSContext in
-        // probably there is a better way to do this... for sure...
-        dispatch_once(&once) {
-            SharedTypeSwiftContext._defaultContext.exceptionHandler = { context, exception in
-                print("JS Error: \(exception)")
-            }
-
-            let fileURL = NSBundle.mainBundle().URLForResource("bundle", withExtension: "js")
-            let bundle = try! String(contentsOfURL: fileURL!, encoding: NSUTF8StringEncoding)
-
-            SharedTypeSwiftContext._defaultContext.evaluateScript(bundle)
+    private init() {
+        SharedTypeSwiftContext._defaultContext.exceptionHandler = { context, exception in
+            print("JS Error: \(exception)")
         }
-        return SharedTypeSwiftContext._defaultContext
+
+        let fileURL = NSBundle.mainBundle().URLForResource("bundle", withExtension: "js")
+        let bundle = try! String(contentsOfURL: fileURL!, encoding: NSUTF8StringEncoding)
+        context.evaluateScript(bundle)
     }
 }
